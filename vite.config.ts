afb03433969738f8c7cd7225e3e361908c467205
Leena <leenabintmohammed200@gmail.com>
2026-08-22 01:32:16 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
 
-  return {
-    define: {
-      "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
-        env.VITE_SUPABASE_URL
-      ),
-      "import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY": JSON.stringify(
-        env.VITE_SUPABASE_PUBLISHABLE_KEY
-      ),
-    },
 
     resolve: {
       tsconfigPaths: true,
