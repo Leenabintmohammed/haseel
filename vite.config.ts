71d784255a609df4437b8575015aa428a8e3865d
Leena <leenabintmohammed200@gmail.com>
2026-08-22 01:33:29 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,27 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-export default defineConfig(({ mode }) => {
 
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
 
-    resolve: {
-      tsconfigPaths: true,
-    },
+  plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr",
+      },
+    }),
 
-    plugins: [
-      cloudflare({
-        viteEnvironment: {
-          name: "ssr",
-        },
-      }),
-      tailwindcss(),
-      tanstackStart({
-        server: {
-          entry: "server",
-        },
-      }),
-      react(),
-    ],
-  };
+    tailwindcss(),
+
+    tanstackStart({
+      server: {
+        entry: "server",
+      },
+    }),
+
+    react(),
+  ],
 });
